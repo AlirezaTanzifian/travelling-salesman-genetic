56c6647465c8863d817413c162d269b6b2271ea7
Alireza <alireza.tanzifian7@gmail.com>
2022-12-24 11:58:33 +0330

    Improve codes

--- GA.js
+++ GA.js
@@ -7,6 +7,152 @@ document.getElementById("form").addEventListener("submit", function (e) {
     GA(number_of_city);
 });
 
+
+function permutation(n) {
+    let arr = [];
+    for ( let i = 0 ; i < n ; i++ ) {
+        arr[i] = i;
+    }
+    // console.log(arr);
+    let per = [];
+    let step = n;
+    for ( let j = 0 ; j < n ; j++ ) {
+        random_number = Math.floor(Math.random() * step); 
+        // console.log("this is a step : ",step,"this is a random : ",random_number);
+        let temp = arr.splice(random_number,1)[0];
+        per.push(temp);
+        step--;
+    }
+    // console.log(per);
+    return per;
+}
+
+
+function initialization(number_of_population, number_of_city, Distance_between_cities){
+    let population = [];
+    let counter = 0;
+    for ( let i = 0 ; i < number_of_population ; i++) {
+        let chromosome = permutation(number_of_city);
+        // console.log(counter, ">>" ,chromosome);
+        counter++;
+        let existence = new Existence(chromosome, Distance_between_cities);
+        population.push(existence);
+    }
+    return population;
+}
+
+
+class Existence {
+    constructor(chromosome, Distance_between_cities){
+        this.chromosome = chromosome;
+        this.fitness = fitnessFunction(chromosome, Distance_between_cities);
+    }
+}
+
+
+function fitnessFunction(chromosome, Distance_between_cities){
+    fitness = 0;
+    for ( let i = 0 ; i < chromosome.length ; i++ ){
+        fitness += Distance_between_cities[chromosome[i]][chromosome[(i + 1) % chromosome.length]];
+    }
+    // console.log(fitness);
+    return fitness;
+}
+
+
+function parentSelection(population, crossover_probability){
+    // sorting population list
+    for( let i = 0; i < population.length; i++ ){
+        for( let j = i + 1; j < population.length; j++ ){
+            if(population[i].fitness > population[j].fitness){
+                temp = population[i];
+                population[i] = population[j];
+                population[j] = temp;
+            }
+        }
+    }
+    let number_of_parents = crossover_probability * population.length / 100;
+    // select best parents base on fitness
+    let parents = [];
+    for ( let i = 0 ; i < number_of_parents ; i++ ){
+        parents.push(population[i])
+    }
+    // console.log(parents);    
+    return parents;
+}
+
+
+function crossOver(parents, Distance_between_cities) {
+    let offsprings = [];
+    let length_of_parents_list = parents.length;
+    for ( let i = 0 ; i < length_of_parents_list ; i+=2 ) {
+        let parent1 = parents[i].chromosome;
+        let parent2 = parents[i + 1].chromosome;
+        node = Math.floor(Math.random() * parent1.length);
+        // console.log('random node',node);
+        let offspring2 = parent1.slice(0, node);
+        let offspring1 = parent2.slice(0, node);
+        // console.log("node : ", node);
+        // console.log("parent1", parent1);
+        // console.log("offspring1", offspring1);
+        // console.log("parent2", parent2);
+        // console.log("offspring2", offspring2);
+        let length_of_parent = parent1.length;
+        for ( let j = 0 ; j < length_of_parent ; j++ ) {
+            if (!offspring1.includes(parent1[j])) {
+                offspring1.push(parent1[j]);
+            }
+            if (!offspring2.includes(parent2[j])) {
+                offspring2.push(parent2[j]);
+            }
+        }
+        child1 = new Existence(offspring1, Distance_between_cities);
+        child2 = new Existence(offspring2, Distance_between_cities);
+        offsprings.push(child1);
+        offsprings.push(child2);
+    }
+    // console.log(offsprings);
+    return offsprings;
+}
+
+
+function mutation(offsprings, mutation_probability, Distance_between_cities){
+    for ( let i = 0 ; i < offsprings.length ; i++ ) {
+        random_number = Math.floor(Math.random() * 100);
+        if (random_number < mutation_probability) {
+            // console.log(offsprings[i].chromosome);
+            m = Math.floor(Math.random() * offsprings[i].chromosome.length);
+            n = Math.floor(Math.random() * offsprings[i].chromosome.length);
+            // console.log("old chromosome",offsprings[i].chromosome);
+            // console.log("old fitness",offsprings[i].fitness);
+            let temp = offsprings[i].chromosome[m];
+            offsprings[i].chromosome[m] = offsprings[i].chromosome[n];
+            offsprings[i].chromosome[n] = temp;
+            // console.log("m : ",m,"n : ",n);
+            // console.log("temp",temp);
+            // console.log("new m",offsprings[i].chromosome[m]);
+            // console.log("new n",offsprings[i].chromosome[n]);
+            // console.log(offsprings[i].chromosome);
+            new_value_of_offspring = new Existence(offsprings[i].chromosome, Distance_between_cities);
+            offsprings[i] = new_value_of_offspring;
+            // console.log("new chromosome",offsprings[i].chromosome);
+            // console.log("new fitness",offsprings[i].fitness);
+        }
+    }
+    // console.log("mutation list",offsprings);
+    return offsprings;
+}
+
+
+function serviveSelection(population, number_of_population) {
+    population.sort((a,b) =>{
+        return a.fitness - b.fitness;
+    })
+    population = population.slice(0, number_of_population)
+    return population;
+}
+
+
 function GA(number_of_city){
     if (number_of_city > 0){
         let number_of_population = 500;
@@ -34,142 +180,6 @@ function GA(number_of_city){
     // console.log(Distance_between_cities);
     console.log( Distance_between_cities.join('\n') );
     
-    function permutation(n) {
-        let arr = [];
-        for ( let i = 0 ; i < n ; i++ ) {
-            arr[i] = i;
-        }
-        // console.log(arr);
-        let per = [];
-        let step = n;
-        for ( let j = 0 ; j < n ; j++ ) {
-            random_number = Math.floor(Math.random() * step); 
-            // console.log("this is a step : ",step,"this is a random : ",random_number);
-            let temp = arr.splice(random_number,1)[0];
-            per.push(temp);
-            step--;
-        }
-        // console.log(per);
-        return per;
-    }
-    
-    class Existence {
-        constructor(chromosome, Distance_between_cities){
-            this.chromosome = chromosome;
-            this.fitness = fitnessFunction(chromosome, Distance_between_cities);
-        }
-    }
-    
-    function fitnessFunction(chromosome, Distance_between_cities){
-        fitness = 0;
-        for ( let i = 0 ; i < chromosome.length ; i++ ){
-            fitness += Distance_between_cities[chromosome[i]][chromosome[(i + 1) % chromosome.length]];
-        }
-        // console.log(fitness);
-        return fitness;
-    }
-    
-    function initialization(number_of_population, number_of_city, Distance_between_cities){
-        let population = [];
-        let counter = 0;
-        for ( let i = 0 ; i < number_of_population ; i++) {
-            let chromosome = permutation(number_of_city);
-            // console.log(counter, ">>" ,chromosome);
-            counter++;
-            let existence = new Existence(chromosome, Distance_between_cities);
-            population.push(existence);
-        }
-        return population;
-    }
-    
-    function parentSelection(population, crossover_probability){
-        // sorting population list
-        for( let i = 0; i < population.length; i++ ){
-            for( let j = i + 1; j < population.length; j++ ){
-                if(population[i].fitness > population[j].fitness){
-                    temp = population[i];
-                    population[i] = population[j];
-                    population[j] = temp;
-                }
-            }
-        }
-        let number_of_parents = crossover_probability * population.length / 100;
-        // select best parents base on fitness
-        let parents = [];
-        for ( let i = 0 ; i < number_of_parents ; i++ ){
-            parents.push(population[i])
-        }
-        // console.log(parents);    
-        return parents;
-    }
-    
-    function crossOver(parents, Distance_between_cities) {
-        let offsprings = [];
-        let length_of_parents_list = parents.length;
-        for ( let i = 0 ; i < length_of_parents_list ; i+=2 ) {
-            let parent1 = parents[i].chromosome;
-            let parent2 = parents[i + 1].chromosome;
-            node = Math.floor(Math.random() * parent1.length);
-            // console.log('random node',node);
-            let offspring2 = parent1.slice(0, node);
-            let offspring1 = parent2.slice(0, node);
-            // console.log("node : ", node);
-            // console.log("parent1", parent1);
-            // console.log("offspring1", offspring1);
-            // console.log("parent2", parent2);
-            // console.log("offspring2", offspring2);
-            let length_of_parent = parent1.length;
-            for ( let j = 0 ; j < length_of_parent ; j++ ) {
-                if (!offspring1.includes(parent1[j])) {
-                    offspring1.push(parent1[j]);
-                }
-                if (!offspring2.includes(parent2[j])) {
-                    offspring2.push(parent2[j]);
-                }
-            }
-            child1 = new Existence(offspring1, Distance_between_cities);
-            child2 = new Existence(offspring2, Distance_between_cities);
-            offsprings.push(child1);
-            offsprings.push(child2);
-        }
-        // console.log(offsprings);
-        return offsprings;
-    }
-    
-    function mutation(offsprings, mutation_probability, Distance_between_cities){
-        for ( let i = 0 ; i < offsprings.length ; i++ ) {
-            random_number = Math.floor(Math.random() * 100);
-            if (random_number < mutation_probability) {
-                // console.log(offsprings[i].chromosome);
-                m = Math.floor(Math.random() * offsprings[i].chromosome.length);
-                n = Math.floor(Math.random() * offsprings[i].chromosome.length);
-                // console.log("old chromosome",offsprings[i].chromosome);
-                // console.log("old fitness",offsprings[i].fitness);
-                let temp = offsprings[i].chromosome[m];
-                offsprings[i].chromosome[m] = offsprings[i].chromosome[n];
-                offsprings[i].chromosome[n] = temp;
-                // console.log("m : ",m,"n : ",n);
-                // console.log("temp",temp);
-                // console.log("new m",offsprings[i].chromosome[m]);
-                // console.log("new n",offsprings[i].chromosome[n]);
-                // console.log(offsprings[i].chromosome);
-                new_value_of_offspring = new Existence(offsprings[i].chromosome, Distance_between_cities);
-                offsprings[i] = new_value_of_offspring;
-                // console.log("new chromosome",offsprings[i].chromosome);
-                // console.log("new fitness",offsprings[i].fitness);
-            }
-        }
-        // console.log("mutation list",offsprings);
-        return offsprings;
-    }
-    
-    function serviveSelection(population, number_of_population) {
-        population.sort((a,b) =>{
-            return a.fitness - b.fitness;
-        })
-        population = population.slice(0, number_of_population)
-        return population;
-    }
     
     let Population = initialization(number_of_population, number_of_city, Distance_between_cities);
     // console.log(Population);
